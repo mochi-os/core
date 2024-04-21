ddace2aa81a852fa0563403fb65fcfdff3262af4
Alistair Cunningham <acunningham@integrics.com>
2024-04-21 11:11:30 +0200

    Chat

--- server/chat.go
+++ server/chat.go
@@ -1,4 +1,4 @@
-// Comms sample internal app: Chat
+// Comms: Chat
 // Copyright Alistair Cunningham 2024
 
 package main
@@ -8,6 +8,7 @@ func init() {
 	app_register_action("chat", "", chat_list)
 	app_register_action("chat", "list", chat_list)
 	app_register_action("chat", "new", chat_new)
+	app_register_action("chat", "send", chat_send)
 	app_register_action("chat", "view", chat_view)
 	app_register_event("chat", "message", chat_message_receive)
 	app_register_path("chat", "chat")
@@ -60,6 +61,32 @@ func chat_new(u *User, action string, format string, p app_parameters) string {
 	return app_template("chat/"+format+"/new", friends)
 }
 
+// Send a chat message
+func chat_send(u *User, action string, format string, p app_parameters) string {
+	//TODO Sort out pointers
+	fp, _ := service_generic[*Object](u, "friends", "get", app_parameter(p, "friend", ""))
+	f := *fp
+	if f == nil {
+		return "Friend not found"
+	}
+	c := object_by_name(u, "chat", "friend", f.Name)
+	if c == nil {
+		c = object_create(u, "chat", "friend", f.Name, f.Label)
+		if c == nil {
+			return "Unable to create chat"
+		}
+	} else {
+		object_touch(u, c.ID)
+	}
+
+	message := app_parameter(p, "message", "")
+	event(u, f.Name, "chat", "", "message", json_encode(map[string]string{"body": message}))
+	j := json_encode(map[string]string{"from": u.Public, "name": u.Name, "time": time_unix_string(), "body": message})
+	object_value_append(u, c.ID, "messages", "\n"+j)
+	websockets_send(u, "chat", j)
+	return ""
+}
+
 // View a chat
 func chat_view(u *User, action string, format string, p app_parameters) string {
 	//TODO Sort out pointers
@@ -80,15 +107,5 @@ func chat_view(u *User, action string, format string, p app_parameters) string {
 	}
 
 	messages := object_value_get(u, c.ID, "messages", "")
-	message := app_parameter(p, "message", "")
-	if message != "" {
-		// User sent a message
-		event(u, f.Name, "chat", "", "message", json_encode(map[string]string{"body": message}))
-		j := json_encode(map[string]string{"from": u.Public, "name": u.Name, "time": time_unix_string(), "body": message})
-		object_value_append(u, c.ID, "messages", "\n"+j)
-		messages = messages + "\n" + j
-		websockets_send(u, "chat", j)
-	}
-
 	return app_template("chat/"+format+"/view", map[string]any{"Friend": f, "Messages": messages})
 }

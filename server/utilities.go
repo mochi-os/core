53ec17ffe9f22bdcb86be7d913eb43d1dc462613
Alistair Cunningham <acunningham@integrics.com>
2025-11-28 17:28:41 +0900

    Access and groups

--- server/utilities.go
+++ server/utilities.go
@@ -326,7 +326,7 @@ func valid(s string, match string) bool {
 
 	switch match {
 	case "action":
-		match = "^[0-9a-zA-Z/-:]{1,100}$"
+		match = "^[0-9a-zA-Z/-:_]{1,100}$"
 	case "constant":
 		match = "^[0-9a-zA-Z/\\-\\._]{1,100}$"
 	case "entity":
